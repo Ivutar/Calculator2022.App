8164938944e756879588ccaef24cf8b133b026fe
ast <atrusov@gmail.com>
2022-12-08 07:41:35 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Calculator2022;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Calculator2022;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
